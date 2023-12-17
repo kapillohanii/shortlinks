167f0c08954c0304e2745a649c439743e4b8e467
kapillohanii <kapillohani023@gmail.com>
2023-12-17 17:47:27 +0530

    server

--- server/middleware.js
+++ server/middleware.js
@@ -3,6 +3,7 @@ const isAuthenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
+    console.log("unauthorized!");
     res.status(401).json({ message: 'Unauthorized' });
 };
 
